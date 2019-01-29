252fda3c06647d72619dd2910f71a68eded9032c
Digicrest <kaylaniwalker@gmail.com>
2019-01-29 23:51:17 +0000

    29/01 - Easy - Additive Persistence
    How many iterations of summing the digits of N are required to end up with a single digit.

--- daily-programmer/easy/additive-persistence.js
+++ daily-programmer/easy/additive-persistence.js
@@ -1,11 +1,9 @@
-let counter = 0;
-
-const additivePersistence = n => {
+const additivePersistence = (n, count = 0) => {
     const digits = n.toString().split("").map(d => Number(d));
 
-    if (digits.length <= 1) return counter;
-    counter++;
-    return additivePersistence(digits.reduce((sum, num) => sum += num, 0))
+    return digits.length <= 1
+        ? count
+        : additivePersistence(digits.reduce((sum, num) => sum += num, 0), count++)
 }
 
 console.log(additivePersistence(199));
